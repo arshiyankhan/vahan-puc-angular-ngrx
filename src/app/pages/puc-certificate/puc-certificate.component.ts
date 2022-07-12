f17c6db4500e3858b48eea692df0d58139adf51f
Piyush <piyushk1264@gmail.com>
2022-07-12 20:46:05 +0530

    PUC Certificate UI Done

--- src/app/pages/puc-certificate/puc-certificate.component.ts
+++ src/app/pages/puc-certificate/puc-certificate.component.ts
@@ -5,10 +5,10 @@ import { MatStepper } from '@angular/material/stepper';
 @Component({
   selector: 'app-puc-certificate',
   templateUrl: './puc-certificate.component.html',
-  styleUrls: ['./puc-certificate.component.css']
+  styleUrls: ['./puc-certificate.component.scss']
 })
 export class PucCertificateComponent implements OnInit {
-
+  isDarkModeActive =  false;
   constructor(
     private _formBuilder: FormBuilder
   ) { }
@@ -18,10 +18,28 @@ export class PucCertificateComponent implements OnInit {
     vehicleRegistrationNumber: ['', Validators.required],
   });
 
+  userDetailsRightForm = this._formBuilder.group({
+    regNumber: ['', Validators.required],
+    regOwner: ['', Validators.required],
+    ownerPhoneNumber: ['', Validators.required],
+    markersClass: ['', Validators.required],
+    vehicleClass: ['', Validators.required],
+    monthYearOfMfg: ['', Validators.required],
+    fuelUsed: ['', Validators.required],
+    chasisNumber: ['', Validators.required],
+    engineNumber: ['', Validators.required],
+    dateOfReg: ['', Validators.required],
+    engineStroke: ['', Validators.required],
+    emissionNorms: ['', Validators.required],
+    odometerReading: ['', Validators.required],
+    insuranceCertNumber: ['', Validators.required],
+    insuranceCompany: ['', Validators.required],
+    insuranceValidity: ['', Validators.required],
+  });
+
   otp: string = ""
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onOtpChange(otp: string) {
     this.otp = otp
@@ -30,5 +48,11 @@ export class PucCertificateComponent implements OnInit {
   verifyOtp(stepper: MatStepper) {
     console.log(this.otp)
     stepper.next();
+  }
+
+  changeValueExample() {
+    this.userDetailsRightForm.patchValue({
+      regNumber: 'TN-12-12-12',
+    })
   }
 }
